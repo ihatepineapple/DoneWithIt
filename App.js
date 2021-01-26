53ed6c0014ee6a9d1c2d475616b43457158905e6
Irene A <ihatepineapple@gmail.com>
2021-01-26 14:06:08 +0100

    picker built

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from "react";
 import ViewImageScreen from './app/screens/ViewImageScreen';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from "./app/components/Card";
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
@@ -10,10 +10,32 @@ import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem'
 import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen'
+import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from "./app/components/AppPicker";
+
+
+const categories =[
+  {label: "Furniture", value: 1},
+  {label: "Clothing", value: 2},
+  {label: "Cameras", value: 3},
+]
 
 export default function App() {
-  return <ListingsScreen />
+  const [category, setCategory] = useState();
+
+  return (
+    <Screen>
+      <AppPicker 
+      selectedItem={category}
+      onSelectItem={item => setCategory(item)}
+      items={categories} placeholder="category" icon="apps"/>
+      <AppTextInput placeholder="username" icon="email" />
+
+    </Screen>
+  
+  
+    )
   
 }
 
