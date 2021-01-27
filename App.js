757d29d01bf259c662cb3483b6832ff38926f378
Irene A <ihatepineapple@gmail.com>
2021-01-27 16:39:14 +0100

    image picker added

--- App.js
+++ App.js
@@ -18,37 +18,13 @@ import AppPicker from "./app/components/AppPicker";
 import LoginScreen from "./app/screens/LoginScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import * as ImagePicker from "expo-image-picker";
+import ImageInputList from "./app/components/ImageInputList";
 
 
 
 export default function App() {
-  const [imageUri, setImageUri] = useState()
-
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted)
-      alert("You need to enable permission to access the library")
-  }
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled)
-        setImageUri(result.uri);
-    } catch (error) {
-      console.log("Error while reading an image")
-    }
-  }
-
-  useEffect(() => {
-    requestPermission();
-  }, [])
- 
-  return <Screen>
-    <AppButton title="Select Image" onPress={selectImage} />
-    <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-    <ImageInput imageUri={imageUri} />
-  </Screen>
+  
+  return <ListingEditScreen />
 
   
 }
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -9,6 +9,7 @@ import {
   AppFormPicker as Picker,
   SubmitButton,
 } from "../components/forms";
+import FormImagePicker from "../components/forms/FormImagePicker";
 import Screen from "../components/Screen";
 
 const validationSchema = Yup.object().shape({
@@ -16,6 +17,7 @@ const validationSchema = Yup.object().shape({
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image")
 });
 
 const categories = [
@@ -84,10 +86,12 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
+          images: [],
         }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
+        <FormImagePicker name="images" />
         <FormField maxLength={255} name="title" placeholder="Title" />
         <FormField
           keyboardType="numeric"
